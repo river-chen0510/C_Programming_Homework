4691052067d5cc0d96d09a745e73970758c365e6
River Chen <riverchen0510@gmail.com>
2025-12-17 22:27:24 -0800

    question 7 not working

--- 19-String and Character Arrays/Assignments/Question07.c
+++ 19-String and Character Arrays/Assignments/Question07.c
@@ -2,15 +2,20 @@
 #include <string.h>
 int main(){
     char arr[] = {"hello world"};
+
     int temp = 0;
-    int size = strlen(arr);
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    signed char arr2[size];
     
     for(int i = 0; i < size; i++){
-        temp = arr[i];
+        arr[i] = arr2[i];
+        temp = arr2[i];
         temp - 32;
-        temp = arr[i];
+        temp = arr2[i];
 
-        printf("%c", arr[i]);
+        printf("%c", arr2[i]);
+        
     }
 
     return 0;
